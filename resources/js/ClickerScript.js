e0790a9cbe6055ca68594c06c559ac5677afb52b
Daniel-Mathiesen <6249585@rts-365.dk>
2021-08-31 15:16:55 +0200

    Add click script function

--- resources/js/ClickerScript.js
+++ resources/js/ClickerScript.js
@@ -1,5 +1,9 @@
 console.log("Hello :P")
+var AldiCount = 0
+var display = document.getElementById("PointsCounter");
 
 document.getElementById("BigAldiClicker").addEventListener("click", function(clickEvent){
     console.log("You Pressed Meh :D")
+    AldiCount++;
+    display.innerHTML = AldiCount;
 });
