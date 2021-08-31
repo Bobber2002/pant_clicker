54f0e953cf05f187b3c76a15cbf6ec66ab5ae1dc
ChristofferRTS <4906240@rts-365.dk>
2021-08-31 15:24:28 +0200

    Add funtionality to decrease aldicount

---
+++ resources/js/BuyingBuilding.js
@@ -0,0 +1,11 @@
+
+var price = 10;
+
+
+document.getElementById("building").addEventListener("click", function(clickEvent){
+    console.log("You Pressed Meh :D")
+    AldiCount -= price;
+    display.innerHTML = AldiCount;
+});
+
+
--- resources/js/ClickerScript.js
+++ resources/js/ClickerScript.js
@@ -2,8 +2,11 @@ console.log("Hello :P")
 var AldiCount = 0
 var display = document.getElementById("PointsCounter");
 
+
 document.getElementById("BigAldiClicker").addEventListener("click", function(clickEvent){
     console.log("You Pressed Meh :D")
     AldiCount++;
     display.innerHTML = AldiCount;
 });
+
+
