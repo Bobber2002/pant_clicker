df57d6224c12aa0d8f1522ebb0967cf8c797cd3d
Daniel-Mathiesen <6249585@rts-365.dk>
2021-09-02 14:05:57 +0200

    Add funny console comments

--- resources/js/ClickerScript.js
+++ resources/js/ClickerScript.js
@@ -1,5 +1,6 @@
 console.log("Hello :P");
-
+console.log("--------");
+console.log("Also what are you doing here O-O*");
 document.getElementById("BigAldiClicker").addEventListener("click", function(clickEvent){
     AldiCount+=StartClickValue;
     display.innerHTML = AldiCountShown;
