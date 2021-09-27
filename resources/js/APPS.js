80430c47adc939b300c73e9d187eeead02843917
Daniel Mathiesen <danielverdugomathiesen@gmail.com>
2021-09-27 14:09:20 +0200

    Add fix for title

--- resources/js/APPS.js
+++ resources/js/APPS.js
@@ -4,7 +4,7 @@
     AldiCount += apps/10;
     var perSecond = document.getElementById("perSecondCounter")  
         perSecond.innerHTML = apps.toFixed(1);
-        document.title=(AldiCount).toFixed()+' Aldi points - Cookie Clicker';
+        document.title=(AldiCount).toFixed()+' Aldi points - Aldi Clicker';
     loop()
   }, 100); 
 }());
