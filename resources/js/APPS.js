26d13943ce782476f92f386e72d8331d98fc954e
ChristofferRTS <4906240@rts-365.dk>
2021-10-11 00:24:17 +0200

    add temp fix to apps

--- resources/js/APPS.js
+++ resources/js/APPS.js
@@ -1,10 +1,18 @@
 
+// (function loop() {
+//   setTimeout(function () {
+//     AldiCount += apps/10;
+//     var perSecond = document.getElementById("perSecondCounter")  
+//         perSecond.innerHTML = apps.toFixed(1);
+//         document.title=(AldiCount).toFixed()+' Aldi points - Aldi Clicker';
+//     loop()
+//   }, 100); 
+// }());
+
+
 (function loop() {
   setTimeout(function () {
-    AldiCount += apps/10;
-    var perSecond = document.getElementById("perSecondCounter")  
-        perSecond.innerHTML = apps.toFixed(1);
-        document.title=(AldiCount).toFixed()+' Aldi points - Aldi Clicker';
+      AldiCount += apps/10;
     loop()
   }, 100); 
 }());
